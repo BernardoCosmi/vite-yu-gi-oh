01490586d57a3e8eaaf13a183bec6ad850d8a5e9
BernardoCosmi <bernardo.daddo90@gmail.com>
2024-02-12 16:39:08 +0100

    style: added "card found" banner

--- src/components/cards/CardList.vue
+++ src/components/cards/CardList.vue
@@ -1,4 +1,9 @@
 <template>
+    <div class="bg-dark text-light text-center p-2 my-3">
+        <h3>
+            Found {{ store.cardsArray.length }} cards
+        </h3>
+    </div>
     <div class="row">
         <SingleCard v-for="(element, index) in store.cardsArray" :key="index" :propsElement="element" />
     </div>
